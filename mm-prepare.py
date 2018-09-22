346eb2e7fc458db89055cb52241548552c71d9d1
osgav <osgav0@gmail.com>
2018-09-22 16:31:31 +0100

    prepare all data files without importing py files

--- mm-prepare.py
+++ mm-prepare.py
@@ -3,23 +3,33 @@
 # osgav
 # mm-prepare.py
 #
-# 1 turn .lua files into .py files
-# 2 import .py files 
-# 3 run slpp against imported data
-# 4 save data to .json files
+# 1. open .lua file
+# 2. decode lua table
+# 3. write as json to new file
+# 4. repeat
 #
 # https://stackoverflow.com/questions/39838489/converting-lua-table-to-a-python-dictionary
 # pip install git+https://github.com/SirAnthony/slpp
 #
 
-# step 2 and 3
-from MM00Data import MM00DataSavedVariables
-from slpp import slpp 
-tada = slpp.decode(MM00DataSavedVariables)
-
-# step 4
+import os
 import json
-with open('MM00Data.json', 'w') as handle:
-    handle.write(json.dumps(tada))
+
+from slpp import slpp
 
 
+MM_DATA_PATH = "../2018-09-22-datacopy"
+# this folder should contain your MM00Data.lua, MM01Data.lua, etc files
+
+for file in os.listdir(MM_DATA_PATH):
+
+    if "Data.lua" in file:
+        
+        with open('%s/%s' % (MM_DATA_PATH, file), 'r') as handle:
+            luafile = handle.readlines()
+        luastring = "".join(luafile[1:])
+        tada = slpp.decode(luastring)
+
+        jsonfile = file.replace('lua', 'json')
+        with open('%s/json/%s' % (MM_DATA_PATH, jsonfile), 'w') as handle:
+            handle.write(json.dumps(tada))
